ee38ad605e57aa3326254c88b25a1b197013d20c
Galtrips <galtrips.pvp@gmail.com>
2023-04-03 10:44:55 +0200

    Delete for bid

--- backend/routes/bid.js
+++ backend/routes/bid.js
@@ -1,11 +1,41 @@
 import authMiddleware from '../middlewares/auth.js'
-import { Bid, Product } from '../orm/index.js'
+import { Bid, Product, User } from '../orm/index.js'
 import express from 'express'
 import { getDetails } from '../validators/index.js'
 
 const router = express.Router()
 
-router.delete('/api/bids/:bidId', async (req, res) => {
+router.delete('/api/bids/:bidId', authMiddleware, async (req, res) => {
+
+  try {
+
+    let { bidId } = req.params;
+
+    let data = await Bid.findOne(
+      {
+        where: { id: bidId },
+        include: [{
+          model: User,
+          as: 'bidder',
+          attributes: ['id', 'username', 'admin']
+        }]
+      }
+    )
+
+    if (!data) {
+      res.status(404).send()
+    } else if (data.bidderId !== req.user.id && !req.user.admin) {
+      res.status(403).send()
+    } else {
+      res.status(204).json(await Bid.destroy(
+        { where: { id: bidId } }
+      ))
+    }
+
+  } catch (err) {
+    res.status(400).json({ error: "Invalid or missing fields", details: err })
+  }
+
   res.status(600).send()
 })
 
