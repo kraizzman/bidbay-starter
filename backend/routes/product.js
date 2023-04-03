2b050d20e13e8aa77142fba305959e8a50c84683
Galtrips <galtrips.pvp@gmail.com>
2023-04-03 10:16:40 +0200

    Put endPoint

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -74,7 +74,49 @@ router.post('/api/products', authMiddleware, async (req, res) => {
   res.status(600).send()
 })
 
-router.put('/api/products/:productId', async (req, res) => {
+router.put('/api/products/:productId', authMiddleware, async (req, res) => {
+
+  try {
+
+    let { productId } = req.params;
+
+    req.body.sellerId = req.user.id;
+
+    let data = await Product.findOne(
+      {
+        where: { id: productId },
+        include: [{
+          model: User,
+          as: 'seller',
+          attributes: ['id', 'username', 'admin']
+        }, {
+          model: Bid,
+          as: 'bids',
+          attributes: ['id', 'price', 'date'],
+          include: [{
+            model: User,
+            as: 'bidder',
+            attributes: ['id', 'username']
+          }]
+        }]
+      }
+    )
+
+    if (!data) {
+      res.status(404).send()
+    } else if (data.sellerId !== req.user.id && !req.user.admin) {
+      res.status(403).send()
+    } else {
+      res.status(200).json(await Product.update(
+        req.body,
+        { where: { id: productId } }
+      ))
+    }
+
+  } catch (error) {
+    res.status(400).json({ error: "Invalid or missing fields", details: error })
+  }
+
   res.status(600).send()
 })
 
